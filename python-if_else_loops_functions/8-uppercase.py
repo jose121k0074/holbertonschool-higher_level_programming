778c91c270d5b29ee3ebc499b2618d1f50d4bc88
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-29 07:37:52 -0800

    uppercase print

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    temp = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            temp += chr(ord(str[i]) - 32)
+            continue
+        temp += str[i]
+    print('{0}'.format(temp))
