42e23aa5ab5ecf85429947c60be4e3b303daac65
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-29 10:47:55 -0800

    fix bugs 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,18 +2,20 @@
 import sys
 
 if __name__ == '__main__':
-    """Prints the argument list passed to the program
     """
-     argList = sys.argv
-     lenList = len(argList) - 1
-     if lenList > 1:
-         print(lenList, 'arguments:')
-         for i in range(1, lenList + 1):
-             print('{:d}: {}'.format(i, argList[i]))
+    Prints the argument list passed to the program
+
+    """
+    argList = sys.argv
+    lenList = len(argList) - 1
+
+    if lenList > 1:
+        print(lenList, 'arguments:')
+        for i in range(1, lenList + 1):
+            print('{:d}: {}'.format(i, argList[i]))
     elif lenList == 1:
         print(lenList, 'argument:')
         for i in range(1, lenList + 1):
             print('{:d}: {}'.format(i, argList[i]))
     elif lenList == 0:
         print(lenList, 'arguments.')
-
