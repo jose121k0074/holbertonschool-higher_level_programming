a75328640394dc80235f31309b78178829dcc1d2
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-04 20:25:41 -0800

    fix bugs 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-
-    try:
-        for i in my_list:
-            if i < x:
-                print('{}'.format(my_list[i]), end='')
-                i += 1
-
-        print()
-    except TypeError:
-        pass
-    finally:
-        return i
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+        except IndexError:
+            break
+    print("")
+    return i
