1016d4f0b332590b40d47394a444a6a83ae0b0aa
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-05 09:28:19 -0800

    fix bug 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,32 +16,11 @@ def add_integer(a, b=98):
 
     """
 
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-
-    a = convert_to_int(a)
-    b = convert_to_int(b)
-    return a + b
-
-
-def convert_to_int(num):
-    """Cast the data type of num parameter
-
-    Convert a float number to a integer number
-
-    Args:
-        num (:obj:`int, float`): The number to cast.
-
-    Returns:
-        int: The number casted to integer.
-
-    """
-
-    if type(num) is float:
-        num = int(num)
-        return num
-
-    return num
+    a = int(a)
+    b = int(b)
+    return (a + b)
