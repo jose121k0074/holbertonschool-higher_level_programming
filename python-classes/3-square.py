d48b5e037e581dd608b24f76778c5c77a5636ecd
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-04 21:26:02 -0800

    area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+class Square:
+    """represents a Square"""
+
+    def __init__(self, size=0):
+        """Initializes a new Square
+
+
+        Args:
+            size (int): The size of the new square.
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area
+
+        """
+        return self.__size ** 2
