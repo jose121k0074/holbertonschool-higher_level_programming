398d0faa6acccf067c417f384c59da17b7761f89
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 21:50:01 -0800

    iprove geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""geometry class"""
+
+
+class BaseGeometry:
+    """
+    
+    An empty Rectangle class
+
+    """
+
+    def area(self):
+    	"""
+    	
+    	initializes some values
+    	
+    	"""
+        
+        raise Exception('area() is not implemented')
