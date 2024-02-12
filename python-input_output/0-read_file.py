3f843a6063fba5f00dd7a6831cacaff363d8a8c3
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 22:53:40 -0800

    fix bug 1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""read file"""
+
+
 def read_file(filename=""):
+    """
+    init function
+    """
+
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
