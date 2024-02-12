a92dc8d041f1f048584ce4181cb7c5741ca6cd61
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 21:29:25 -0800

    fix bug 3

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+"""define my list"""
+
+
 def is_same_class(obj, a_class):
-    """
-    Checks if `obj` is exactly an instance of the specified class
-
-    Args:
-        obj (any): The object to compare
-        a_class (any): The class to compare with the object
-
-    Returns:
-        `True` if the object is exactly an instance of the
-        specified class; otherwise `False`
-    """
+    """define my list"""
 
     return type(obj) is a_class
