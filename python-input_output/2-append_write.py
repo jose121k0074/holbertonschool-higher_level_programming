fa1b5937785db9ee6ca9975568d45ec8ab35a8d2
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-12 08:50:53 -0800

    append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Append function"""
+
+
+def append_write(filename="", text=""):
+	"""
+	init function
+	"""
+	
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
