26bd3b2ebba01399f5e2d6cb1d51ee24616ae841
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 22:31:17 -0800

    square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""geometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    
+    Rectangle class
+    
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
