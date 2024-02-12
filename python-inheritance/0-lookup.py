5776997927fed2c038b41ea56e56352429ec070f
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 21:09:13 -0800

    fist ejerccices lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module with a LOOKUP"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
+
+    return dir(obj)
