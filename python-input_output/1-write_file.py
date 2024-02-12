0938c9930cb3aaf2f9e21570f685f631f0f06141
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 23:10:20 -0800

    write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function write"""
+
+
+def write_file(filename="", text=""):
+    """
+    init function
+    """
+    
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
