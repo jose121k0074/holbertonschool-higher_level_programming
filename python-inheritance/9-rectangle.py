a8831bd5fd2a99b128225812c56ebd4bd15bca7d
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-11 22:16:03 -0800

    fix bugs 7

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-"""geometry class"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class
-    """
-    
     def __init__(self, width, height):
-    	"""
-    	
-    	initializes some values
-    	
-    	"""
-
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
@@ -24,12 +11,6 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-    	"""
-    	
-    	return area of rectangle
-    	
-    	"""
-    	
         return self.__width * self.__height
 
     def __str__(self):
