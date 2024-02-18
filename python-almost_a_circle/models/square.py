54e522775b751ab00ad5cdfa199de9230a825b0b
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-18 15:58:30 -0800

    and now, the square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A Square class
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Init class Square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        The __init__ method initializes the size value
+        """
+        
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        ...
+        """
+
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+            self.id, self.x, self.y, self.width
+        )
