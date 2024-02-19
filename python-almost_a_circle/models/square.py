455d9925541e836f007fb6c9dc3821c5f82b07a9
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-02-18 16:02:33 -0800

    size square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,3 +27,20 @@ class Square(Rectangle):
         return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
             self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """
+        ...
+        """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        ...
+        """
+
+        self.width = value
+        self.height = value
