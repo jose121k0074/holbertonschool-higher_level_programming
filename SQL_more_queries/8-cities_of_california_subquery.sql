0a47a787a757da4dbff2f3fe6bb66ca838fe209f
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-03-22 03:50:07 +0000

    task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- lists all cities of California in the database hbtn_0d_usa
+SELECT cities.id, cities.name FROM cities
+WHERE state_id =(
+      SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
